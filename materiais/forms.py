d7b1c06a8b978d7921f6857415de3209b4f88463
Onorio de Jesus <onorio@catalpa.io>
2025-04-26 00:19:03 +0900

    calculation tile works

--- materiais/forms.py
+++ materiais/forms.py
@@ -44,21 +44,66 @@ class TileCalculationForm(forms.ModelForm):
             'total_cost': forms.NumberInput(
                 attrs={'class': 'form-control', 'readonly': 'readonly'}
             ),
-            'waste_percentage': forms.NumberInput(attrs={'class': 'form-control'}),
+            'waste_percentage': forms.NumberInput(
+                attrs={'class': 'form-control', 'readonly': 'readonly'}
+            ),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        tile = cleaned_data.get('tile')
+        room_length = cleaned_data.get('room_length')
+        room_width = cleaned_data.get('room_width')
+        room_quantity = cleaned_data.get('room_quantity', 1)
+
+        if tile and room_length and room_width:
+            try:
+                result = tile.calculate_requirements(
+                    room_length, room_width, room_quantity
+                )
+                cleaned_data['total_boxes'] = result['total_boxes']
+                cleaned_data['total_pieces'] = result['total_pieces']
+                cleaned_data['total_cost'] = result['total_cost']
+                cleaned_data['waste_percentage'] = tile.waste_percentage
+            except (ValueError, TypeError) as e:
+                raise forms.ValidationError(
+                    "Error calculating requirements: " + str(e)
+                )
+        else:
+            if not tile:
+                self.add_error('tile', 'This field is required.')
+            if not room_length:
+                self.add_error('room_length', 'This field is required.')
+            if not room_width:
+                self.add_error('room_width', 'This field is required.')
+
+        return cleaned_data
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if 'tile' in self.data and 'room_length' in self.data and 'room_width' in self.data:
-            tile = Tile.objects.get(pk=self.data['tile'])
-            room_length = float(self.data['room_length'])
-            room_width = float(self.data['room_width'])
-            room_quantity = int(self.data.get('room_quantity', 1))
-            result = tile.calculate_requirements(room_length, room_width, room_quantity)
-            self.initial['total_boxes'] = result['total_boxes']
-            self.initial['total_pieces'] = result['total_pieces']
-            self.initial['total_cost'] = result['total_cost']
-            self.initial['waste_percentage'] = tile.waste_percentage
+        self.fields['total_boxes'].required = False
+        self.fields['total_pieces'].required = False
+        self.fields['total_cost'].required = False
+        self.fields['waste_percentage'].required = False
+
+        if (self.is_bound and 'tile' in self.data and 
+            'room_length' in self.data and 'room_width' in self.data):
+            try:
+                tile = Tile.objects.get(pk=self.data['tile'])
+                room_length = float(self.data['room_length'])
+                room_width = float(self.data['room_width'])
+                room_quantity = int(self.data.get('room_quantity', 1))
+
+                result = tile.calculate_requirements(
+                    room_length, room_width, room_quantity
+                )
+
+                self.initial['total_boxes'] = result['total_boxes']
+                self.initial['total_pieces'] = result['total_pieces']
+                self.initial['total_cost'] = result['total_cost']
+                self.initial['waste_percentage'] = tile.waste_percentage
+            except (ValueError, Tile.DoesNotExist):
+                pass
 
 
 class PlywoodForm(forms.ModelForm):
