0f70a08a6376da1dd960d3aaebd8811354212358
Onorio de Jesus <onorio@catalpa.io>
2025-05-06 22:55:48 +0900

    added domain name housestimator.pythonanywhere.com

--- house_estimator/settings.py
+++ house_estimator/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['housestimator.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
