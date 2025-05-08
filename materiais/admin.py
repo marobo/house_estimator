ff5988a46a8e50e421cddf1e3b8cd932f483e3dd
Onorio de Jesus <onorio@catalpa.io>
2025-05-08 11:36:51 +0900

    undo admin.py

--- materiais/admin.py
+++ materiais/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Tile, Plywood, ElectricComponent, Calculation
 
-admin.site.register(Tile)
-admin.site.register(Plywood)
-admin.site.register(ElectricComponent)
+
+@admin.register(Tile)
+class TileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'length', 'width', 'pieces_per_box', 'price_per_box')
+    search_fields = ('name',)
+
+
+@admin.register(Plywood)
+class PlywoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'length', 'width', 'price_per_sheet')
+    search_fields = ('name',)
+
+
+@admin.register(ElectricComponent)
+class ElectricComponentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit_price', 'unit')
+    search_fields = ('name',)
+
 
 @admin.register(Calculation)
 class CalculationAdmin(admin.ModelAdmin):
