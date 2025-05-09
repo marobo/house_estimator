808fc9e27a1fc890dd8db7423ab6b6564a824133
Onorio de Jesus <onorio@catalpa.io>
2025-05-09 17:15:58 +0900

    login works

--- house_estimator/settings.py
+++ house_estimator/settings.py
@@ -79,4 +79,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+LOGIN_REDIRECT_URL = 'materiais:home'
+LOGOUT_REDIRECT_URL = 'login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
