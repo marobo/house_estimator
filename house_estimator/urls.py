f3eae751b09af550e701cecb289cde1862b2c868
Onorio de Jesus <onorio@catalpa.io>
2025-05-09 21:56:57 +0900

    header and footer more feature

--- house_estimator/urls.py
+++ house_estimator/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('materiais.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
