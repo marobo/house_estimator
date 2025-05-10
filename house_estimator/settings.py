68b925599bfeb91fa6219daf7fc801b8b9648f30
Onorio de Jesus <onorio@catalpa.io>
2025-05-10 21:55:30 +0900

    add logo and preview page for social share

--- house_estimator/settings.py
+++ house_estimator/settings.py
@@ -78,6 +78,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 LOGIN_REDIRECT_URL = 'materiais:home'
 LOGOUT_REDIRECT_URL = 'login'
